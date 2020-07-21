5704382ea68d2229b3352fe11d87c6e62ab23f6a
Mestyan <Mestyan@gmail.com>
2020-07-21 19:59:58 -0400

    Update MakeStickyMenu.js

--- js/MakeStickyMenu.js
+++ js/MakeStickyMenu.js
@@ -2,16 +2,16 @@
 window.onscroll = function() {makeStickyMenu()};
 
 // Get the indexesbar
-var navbar = document.getElementById("indexesbar");
+var indexesBar = document.getElementById("indexesBar");
 
 // Get the offset position of the indexesbar
-var sticky = indexesbar.offsetTop;
+var sticky = indexesBar.offsetTop;
 
 // Add the sticky class to the indexesbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function makeStickyMenu() {
   if (window.pageYOffset >= sticky) {
-    indexesbar.classList.add("sticky")
+    indexesBar.classList.add("sticky")
   } else {
-    indexesbar.classList.remove("sticky");
+    indexesBar.classList.remove("sticky");
   }
 }
