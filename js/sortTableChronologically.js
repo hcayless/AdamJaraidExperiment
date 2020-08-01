73af4882df557a3456b16fb304f943532ee6c47d
Mestyan <Mestyan@gmail.com>
2020-07-31 20:48:22 -0400

    separated js sortChronologically

---
+++ js/sortTableChronologically.js
@@ -0,0 +1,22 @@
+
+
+  //Get all the rows in your table into an Array//
+  var myTableArray = [];
+
+$("tei-table#t1 tei-row").each(function() {
+    var arrayOfThisRow = [];
+    var tableData = $(this).find('tei-cell');
+    if (tableData.length > 0) {
+        tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+        myTableArray.push(arrayOfThisRow);
+    }
+});
+
+
+//Run the sort() method on the array, using a compare function that looks at the date.//
+
+myTableArray.sort(function(a, b) {return a.when - b.when})
+
+//3. Add the rows back to the table in the new order.//
+
+document.getElementById("chrono").innerHTML = myTableArray;
