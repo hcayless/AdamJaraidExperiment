46a0a15e39646d8f71c24d369a2d40bf7161e485
Mestyan <Mestyan@gmail.com>
2020-08-02 20:20:05 -0400

    Update sortTableChronologically.js

--- js/sortTableChronologically.js
+++ js/sortTableChronologically.js
@@ -34,9 +34,13 @@ function SortTableChronologically() {
   //Get all the rows in your table into an Array//
   var theArray = document.getElementsByTagName("tei-row");
   //Run the sort() method on the array, using a compare function that looks at the date.//
-  theArray.sort(function(a, b) {
-   return a.when - b.when});
-  }
+  theArray.sort(function(a, b){
+  var x = a.when;
+  var y = b.when;
+  if (x < y) {return -1;}
+  if (x > y) {return 1;}
+  return 0;
+});
   //3. Add the rows back to the table in the new order.//
   document.getElementById("chrono").innerHTML = theArray;
 }
