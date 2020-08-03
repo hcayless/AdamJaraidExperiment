2fe085b8b099bc799f79566141185e0cf6c5b5e1
Mestyan <Mestyan@gmail.com>
2020-08-02 20:09:13 -0400

    created new JS for arrayexperiment

--- js/sortTableChronologically.js
+++ js/sortTableChronologically.js
@@ -1,25 +1,44 @@
+//
+//
+//   //Get all the rows in your table into an Array//
+//   var myTableArray = [];
+//
+// $("tei-table#t1 tei-row").each(function() {
+//     var arrayOfThisRow = [];
+//     var tableData = $(this).find('tei-cell');
+//     if (tableData.length > 0) {
+//         tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+//         myTableArray.push(arrayOfThisRow);
+//     }
+// });
+//
+//
+// //Run the sort() method on the array, using a compare function that looks at the date.//
+//
+// myTableArray.sort(function(x, y) {
+//   let a = new Date(x.when),
+//       b = new Date(y.when);
+//   return a - b;});
+//
+// //3. Add the rows back to the table in the new order.//
+//
+// document.getElementById("chrono").innerHTML = myTableArray;
 
 
+
+
+
+
+
+function SortTableChronologically() {
   //Get all the rows in your table into an Array//
-  var myTableArray = [];
-
-$("tei-table#t1 tei-row").each(function() {
-    var arrayOfThisRow = [];
-    var tableData = $(this).find('tei-cell');
-    if (tableData.length > 0) {
-        tableData.each(function() { arrayOfThisRow.push($(this).text()); });
-        myTableArray.push(arrayOfThisRow);
-    }
-});
-
-
-//Run the sort() method on the array, using a compare function that looks at the date.//
-
-myTableArray.sort(function(x, y) {
-  let a = new Date(x.when),
-      b = new Date(y.when);
-  return a - b;});
-
-//3. Add the rows back to the table in the new order.//
-
-document.getElementById("chrono").innerHTML = myTableArray;
+  var theArray = document.getElementsByTagName("tei-row");
+  //Run the sort() method on the array, using a compare function that looks at the date.//
+  theArray.sort(function(x, y) {
+    var a = new Date(x.when);
+    var b = new Date(y.when);
+   return a - b;});
+  }
+  //3. Add the rows back to the table in the new order.//
+  document.getElementById("chrono").innerHTML = theArray;
+}
