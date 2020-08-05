35b8e2317496aaa93b6c2a408fd3ff510800a660
Mestyan <Mestyan@gmail.com>
2020-08-05 12:23:47 -0400

    Update sortTableChronologically.js

--- js/sortTableChronologically.js
+++ js/sortTableChronologically.js
@@ -11,17 +11,27 @@ function sortTableChronologically() {
   //Run the sort() method on the array, using a compare function that looks at the date.//
   theArray.sort(function(a, b){
   let x = a.querySelector("td:first-of-type tei-date");
+  //tell that lines without date should not be considered
   if (!x) {return -1};
+  //add the when
   x = new Date(x.getAttribute("when"));
 
   let y = b.querySelector("td:first-of-type tei-date");
+  //tell that lines without date should not be considerd
   if (!y) {return 1};
+  //add the when
   y = new Date(y.getAttribute("when"));
 
   if (x < y) {return -1;}
   if (x > y) {return 1;}
   return 0;
 });
-  //3. Add the rows back to the table in the new order.//
-  document.getElementById("chrono").innerHTML = theArray;
+ //
+
+  //Add the rows back to the table in the new order.//
+
+  var table = document.getElementsById("chrono");
+  table.innerHTML = "";
+  theArray.forEach(function(row) {table.appendChild(row)})
+
 }
