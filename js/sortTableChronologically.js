d333bb2b603134eb33b98ce6345305ae9a2e3eba
Mestyan <Mestyan@gmail.com>
2020-08-05 11:29:27 -0400

    added sorttablechrono after ceteicean

--- js/sortTableChronologically.js
+++ js/sortTableChronologically.js
@@ -1,29 +1,3 @@
-//
-//
-//   //Get all the rows in your table into an Array//
-//   var myTableArray = [];
-//
-// $("tei-table#t1 tei-row").each(function() {
-//     var arrayOfThisRow = [];
-//     var tableData = $(this).find('tei-cell');
-//     if (tableData.length > 0) {
-//         tableData.each(function() { arrayOfThisRow.push($(this).text()); });
-//         myTableArray.push(arrayOfThisRow);
-//     }
-// });
-//
-//
-// //Run the sort() method on the array, using a compare function that looks at the date.//
-//
-// myTableArray.sort(function(x, y) {
-//   let a = new Date(x.when),
-//       b = new Date(y.when);
-//   return a - b;});
-//
-// //3. Add the rows back to the table in the new order.//
-//
-// document.getElementById("chrono").innerHTML = myTableArray;
-
 
 
 
