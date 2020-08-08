2dc93a89c6a0be4a00ccb74567148288dae509db
Mestyan <Mestyan@gmail.com>
2020-08-07 20:08:20 -0400

    added if/then for notAfter attribute to chronologically sorting JS

--- js/sortTableChronologically.js
+++ js/sortTableChronologically.js
@@ -5,7 +5,7 @@
 
 
 function sortTableChronologically() {
-  //Get all the rows in your table into an Array//
+  //Get all the rows in the table into an Array//
   var rows = document.querySelectorAll("#t1 tr");
   var theArray = Array.from(rows);
   //Run the sort() method on the array, using a compare function that looks at the date.//
@@ -13,12 +13,20 @@ function sortTableChronologically() {
   let x = a.querySelector("td:first-of-type tei-date");
   //tell that lines without date should not be considered
   if (!x) {return -1};
+  //tell that lines with notAfter should be considered as date
+  if (x.hasAttribute("notAfter")) {
+    x = new Date(x.getAttribute("notAfter"))
+  }
   //add the when
   x = new Date(x.getAttribute("when"));
 
   let y = b.querySelector("td:first-of-type tei-date");
   //tell that lines without date should not be considerd
   if (!y) {return 1};
+  //tell that lines with notAfter should be transformed into a date
+  if (y.hasAttribute("notAfter")) {
+    y = new Date(x.getAttribute("notAfter"))
+  }
   //add the when
   y = new Date(y.getAttribute("when"));
 
