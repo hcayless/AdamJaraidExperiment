b34708df8edf4667d2ff1bb620777ec352be7d28
Mestyan <Mestyan@gmail.com>
2020-08-07 20:23:10 -0400

    changed the order of arrangment in JS

--- js/sortTableChronologically.js
+++ js/sortTableChronologically.js
@@ -13,26 +13,31 @@ function sortTableChronologically() {
   let x = a.querySelector("td:first-of-type tei-date");
   //tell that lines without date should not be considered
   if (!x) {return -1};
-  //tell that lines with notAfter should be considered as date
-  if (x.hasAttribute("notAfter")) {
-    x = new Date(x.getAttribute("notAfter"))
-  }
   //add the when
   x = new Date(x.getAttribute("when"));
 
   let y = b.querySelector("td:first-of-type tei-date");
   //tell that lines without date should not be considerd
   if (!y) {return 1};
-  //tell that lines with notAfter should be transformed into a date
-  if (y.hasAttribute("notAfter")) {
-    y = new Date(x.getAttribute("notAfter"))
-  }
   //add the when
   y = new Date(y.getAttribute("when"));
 
   if (x < y) {return -1;}
   if (x > y) {return 1;}
   return 0;
+
+  //tell that lines with notAfter should be considered as date
+  if (x.hasAttribute("notAfter")) {
+    x = new Date(x.getAttribute("notAfter"))
+  };
+  //tell that lines with notAfter should be transformed into a date
+  if (y.hasAttribute("notAfter")) {
+    y = new Date(x.getAttribute("notAfter"))
+  };
+  if (x < y) {return -1;}
+  if (x > y) {return 1;}
+  return 0;
+
 });
   //Add the rows back to the table in the new order.//
 
