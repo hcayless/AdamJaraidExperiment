8593dd05dc324cd7753b077760593921163784cb
Mestyan <Mestyan@gmail.com>
2020-08-14 21:33:29 -0400

    Update DisplayID.js

--- js/DisplayID.js
+++ js/DisplayID.js
@@ -3,6 +3,6 @@
 function displayID() {
   var x = document.getElementsByTagName("tr")[0].id;
 
-  var newTd = document.getElementById("tr").insertCell(0);
+  var newTd = document.getElementsByTagName("tr").insertCell(0);
     newTd.innerHTML = x;
 }
