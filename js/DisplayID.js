8800b9739d3dec6b16d160639fc94bbc1e3e5caa
Mestyan <Mestyan@gmail.com>
2020-08-15 19:58:15 -0400

    added new code for displayID

--- js/DisplayID.js
+++ js/DisplayID.js
@@ -1,8 +1,16 @@
 
 
-function displayID() {
-  var x = document.getElementsByTagName("tr")[0].id;
-  var newTd = document.getElementsByTagName("tr").insertCell(0);
-  
-    newTd.innerHTML = x;
-}
+
+
+document.querySelectorAll("#t1 tr").forEach(tr => {
+    const td = tr.insertCell(0);
+    td.textContent = tr.id;
+});
+
+
+// function displayID() {
+//   var x = document.getElementsByTagName("tr")[0].id;
+//   var newTd = document.getElementsByTagName("tr").insertCell(0);
+//
+//     newTd.innerHTML = x;
+// }
