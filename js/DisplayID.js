6a64551864d2867f1d48cc9a11c69caaf4bb9708
Mestyan <Mestyan@gmail.com>
2020-08-15 21:01:50 -0400

    Update DisplayID.js

--- js/DisplayID.js
+++ js/DisplayID.js
@@ -5,8 +5,8 @@ var rows = document.querySelectorAll("#t1 tr");
 var theArray = Array.from(rows);
 
 theArray.forEach(tr => {
-    const td = tr.insertCell(0);
-    td.textContent = tr.id;
+  var td = document.getElementsByTagName("td");
+    td:nth-child(4).innerHTML = tr.id;
 })
 }
 
