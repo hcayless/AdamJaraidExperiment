f589028c81a3204cd416695422d4f7af9ec3a482
Mestyan <Mestyan@gmail.com>
2020-08-19 11:22:45 -0400

    displayId added

--- js/DisplayID.js
+++ js/DisplayID.js
@@ -5,9 +5,9 @@ var rows = document.querySelectorAll("#t1 tr");
 var theArray = Array.from(rows);
 
 theArray.forEach(tr => {
-  var td = document.getElementsByTagName("td");
-    td:nth-child(4).innerHTML = tr.id;
-})
+  var td = tr.querySelector("td:nth-child(4)");
+    td.innerHTML += tr.id;
+});
 }
 
 
