5558a50b4d5f93a7fee7128f93c23fac44723968
Mestyan <Mestyan@gmail.com>
2020-08-19 11:36:29 -0400

    added new displayId

---
+++ js/displayId_new.js
@@ -0,0 +1,9 @@
+function dispayId() {
+var rows = document.querySelectorAll("#t1 tr");
+var theArray = Array.from(rows);
+
+theArray.forEach(tr => {
+  var td = tr.querySelector("td:nth-child(4)");
+    td.innerHTML += tr.id;
+});
+}
